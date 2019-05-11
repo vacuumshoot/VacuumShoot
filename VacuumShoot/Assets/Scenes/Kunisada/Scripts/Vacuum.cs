345c69216c60fa3a7cad6f5e770b03f5cf46874f
TakahiroKunisada <hw16a068@oecu.jp>
2019-05-11 17:04:50 +0900

    吸い取る処理を実装

--- VacuumShoot/Assets/Scenes/Kunisada/Scripts/Vacuum.cs
+++ VacuumShoot/Assets/Scenes/Kunisada/Scripts/Vacuum.cs
@@ -5,38 +5,69 @@ using UnityEngine;
 public class Vacuum : MonoBehaviour{
 
     private GameObject vacuumObj = null;
+    private GameObject player = null;
     private float touchTime = 0;
-    bool on = false; 
+    bool touch = false;
+    bool vacuumItem = false;
+    private float vacuumTime = 0;
 
     // Start is called before the first frame update
     void Start(){
+        player = GameObject.FindGameObjectWithTag("Player");        //Playerを探す
         vacuumObj = GameObject.FindGameObjectWithTag("VacuumObject");        //吸い取れるオブジェクトを探す
     }
 
     // Update is called once per frame
     void Update(){
-        if (Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButton(0)){
             //吸い取り時間の開始
-            if (on == true){
-                touchTime += Time.deltaTime;
+            if (touch == true){
+                vacuumTime -= Time.deltaTime;
+                Debug.Log(vacuumTime);
+                if (vacuumTime <= 0){
+                    vacuumItem = true;
+                }
             }
+
             //長押しの判定
             //if (touchTime >= 2){
 
             //}
+
         }
-        //吸い取り時間の初期化
+
         if (Input.GetMouseButtonUp(0)){
-            touchTime = 0;
+            //touchTime = 0;
+        }
+
+        if (vacuumItem == true){
+            //吸いとったアイテムを保持する処理
+            player.GetComponent<ShootStack>().Stack();
+            vacuumItem = false;
+            vacuumTime = 1.0f;
+        }
+
+    }
+
+    //吸い切るまでの時間の取得
+    private void OnTriggerEnter(Collider col){
+        if (col.CompareTag("VacuumObject")){
+            vacuumTime = col.gameObject.GetComponent<Suzuki.SuperItem>().vacuumTime;
         }
     }
+
     //吸い取り可能
-    private void OnCollisionStay(Collision collision){
-        on = true;
+    private void OnTriggerStay(Collider col){
+        if (col.CompareTag("VacuumObject")) {
+            touch = true;
+        }
     }
     //吸い取り不可能
-    private void OnCollisionExit(Collision collision){
-        on = false;
+    private void OnTriggerExit(Collider col){
+        if (col.CompareTag("VacuumObject")){
+            touch = false;
+            vacuumTime = 0;
+        }
     }
 
 }
