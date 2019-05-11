5db906c11c1a4a2d4d7e85cc5ccc78b0f5f126b0
1192tu96@gmail.com <1192tu96@gmail.com>
2019-05-11 23:08:34 +0900

    ボスのスクリプトを作成

---
+++ VacuumShoot/Assets/Suzuki/Scripts/Main/Boss.cs
@@ -0,0 +1,35 @@
+﻿namespace Suzuki
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	/// <summary>
+	/// ボスキャラのスクリプト
+	/// </summary>
+	public class Boss : MonoBehaviour
+	{
+		public int HP;// 体力
+		GameObject player;// プレイヤー情報
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			player = GameObject.Find("Player");
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			CheckHP();
+		}
+
+		void CheckHP()
+		{
+			if(HP<=0)
+			{
+				Destroy(this);
+			}
+		}
+	}
+}
