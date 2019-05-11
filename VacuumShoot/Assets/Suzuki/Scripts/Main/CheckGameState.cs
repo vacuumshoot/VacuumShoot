0de5bdec42d369e3bd7b8ab694621698f317fa56
1192tu96@gmail.com <1192tu96@gmail.com>
2019-05-11 23:47:34 +0900

    各種ステータスを作成

---
+++ VacuumShoot/Assets/Suzuki/Scripts/Main/CheckGameState.cs
@@ -0,0 +1,29 @@
+﻿namespace Suzuki
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	/// <summary>
+	/// ゲームキャラの状態を確認するクラス
+	/// ここでクリア、ゲームオーバーや全体の処理を確認する
+	/// </summary>
+	public class CheckGameState : MonoBehaviour
+	{
+		Boss boss;
+		Vacuum player;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+
+}
---
+++ VacuumShoot/Assets/Suzuki/Scripts/Main/PlayerStates.cs
@@ -0,0 +1,26 @@
+﻿namespace Suzuki
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	/// <summary>
+	/// プレイヤーのHPなどの情報を格納するクラス
+	/// </summary>
+	public class PlayerStates : MonoBehaviour
+	{
+		public int HP;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
