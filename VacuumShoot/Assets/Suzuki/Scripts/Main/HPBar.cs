28cf9fa2b963e2a76c889f981a8cb280cbdb9c70
1192tu96@gmail.com <1192tu96@gmail.com>
2019-05-12 06:27:48 +0900

    色々追加

---
+++ VacuumShoot/Assets/Suzuki/Scripts/Main/HPBar.cs
@@ -0,0 +1,30 @@
+﻿namespace Suzuki
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using UnityEngine.UI;
+
+	/// <summary>
+	/// HPバーを管理するスクリプト
+	/// プレイヤーのHPを見てバーのサイズを変更する
+	/// </summary>
+	public class HPBar : MonoBehaviour
+	{
+		PlayerStates states;
+		Image HPBarImage;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			states = GameObject.Find("Player").GetComponent<PlayerStates>();
+			HPBarImage = gameObject.GetComponent<Image>();
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			HPBarImage.fillAmount = (float)states.HP / states.MaxHP;
+		}
+	}
+}
