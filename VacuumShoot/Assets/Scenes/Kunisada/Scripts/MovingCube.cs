42c9044b06786808201d148df7ac607f4cc22c7a
TakahiroKunisada <hw16a068@oecu.jp>
2019-05-11 14:35:28 +0900

    プレイヤーの移動とカメラの追従

---
+++ VacuumShoot/Assets/Scenes/Kunisada/Scripts/MovingCube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingCube : MonoBehaviour{
+
+    //先ほど作成したJoystick
+    [SerializeField]
+    private Joystick _joystick = null;
+
+    //移動速度
+    private const float SPEED = 0.1f;
+
+    private void Update(){
+        Vector3 pos = transform.position;
+
+        pos.x += _joystick.Position.x * SPEED;
+        pos.y += _joystick.Position.y * SPEED;
+
+        transform.position = pos;
+    }
+
+}
