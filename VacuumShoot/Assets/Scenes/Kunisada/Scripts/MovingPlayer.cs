85fccd6156204d2c56cf7406ff7c889a986f8af0
1192tu96@gmail.com <1192tu96@gmail.com>
2019-05-12 13:19:15 +0900

    移動制限を実装

--- VacuumShoot/Assets/Scenes/Kunisada/Scripts/MovingPlayer.cs
+++ VacuumShoot/Assets/Scenes/Kunisada/Scripts/MovingPlayer.cs
@@ -11,12 +11,39 @@ public class MovingPlayer : MonoBehaviour{
     [SerializeField]
      float SPEED = 0.1f;
 
-    private void Update(){
+	public GameObject movableArea;
+	Vector3 leftDown;// 左下の座標
+	Vector3 rightUp;// 右上の座標
+
+	private void Start()
+	{
+		leftDown = movableArea.transform.position - (movableArea.GetComponent<BoxCollider>().size / 2);
+		rightUp = movableArea.transform.position + (movableArea.GetComponent<BoxCollider>().size / 2);
+	}
+
+	private void Update(){
         Vector3 pos = transform.position;
         //プレイヤーの縦移動
         pos.x += _joystick.Position.x * SPEED;
         //プレイヤーの横移動
         pos.y += _joystick.Position.y * SPEED;
+
+		if(pos.x<leftDown.x)
+		{
+			pos.x = leftDown.x;
+		}
+		else if(pos.x>rightUp.x)
+		{
+			pos.x = rightUp.x;
+		}
+		if(pos.y<leftDown.y)
+		{
+			pos.y = leftDown.y;
+		}
+		else if(pos.y>rightUp.y)
+		{
+			pos.y = rightUp.y;
+		}
 
         transform.position = pos;
     }
