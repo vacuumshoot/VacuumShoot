7a10aea8e59aac599eb08498342acbf9170971cc
1192tu96@gmail.com <1192tu96@gmail.com>
2019-05-12 09:37:25 +0900

    画像追加

---
+++ VacuumShoot/Assets/Suzuki/Scripts/Items/Enemy.cs
@@ -0,0 +1,38 @@
+﻿namespace Suzuki
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class Enemy : SuperItem
+	{
+		public float attackInterval;// 攻撃の間隔
+		float interval;
+
+		protected override void VirtualStart()
+		{
+			interval = attackInterval;
+			base.VirtualStart();
+		}
+
+		// Update is called once per frame
+		protected override void VirtualUpdate()
+		{
+			interval -= 1.0f * Time.deltaTime;
+			AttackCheck();
+			base.VirtualUpdate();
+		}
+
+		// 攻撃の確認
+		void AttackCheck()
+		{
+			if(interval<=0.0f)
+			{
+				// 攻撃
+				Debug.Log("Attack");
+
+				interval = attackInterval;
+			}
+		}
+	}
+}
