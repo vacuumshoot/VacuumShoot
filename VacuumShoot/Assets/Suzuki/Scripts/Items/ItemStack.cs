6fff00784c44f49c13fedb1dbb4542365cde956b
1192tu96@gmail.com <1192tu96@gmail.com>
2019-05-12 15:24:05 +0900

    更新

--- VacuumShoot/Assets/Suzuki/Scripts/Items/ItemStack.cs
+++ VacuumShoot/Assets/Suzuki/Scripts/Items/ItemStack.cs
@@ -5,17 +5,30 @@ using UnityEngine.UI;
 
 public class ItemStack : MonoBehaviour
 {
-	List<GameObject> objects;// スタックするオブジェクト
-	public List<GameObject> stackObjects;// スタック先オブジェクト
+	public List<Image> stackGameObjects;// スタック先オブジェクトの場所格納
+
+	ShootStack vacuumStack;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		vacuumStack = GameObject.Find("Player").GetComponent<ShootStack>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		int count = vacuumStack.VacuumObjects.Count;
+		for(int i= 0;i<count;++i)
+		{
+			stackGameObjects[i].sprite =
+				vacuumStack.VacuumObjects[i].GetComponent<SpriteRenderer>().sprite;
+		}
+
+	}
+
+	void ObjectStack()
+	{
+
+	}
 }
